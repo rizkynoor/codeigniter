2cc3e206b7958b5acee4760041a18ea84388c0d4
rizkynoor <rizkynoor75@gmail.com>
2016-06-08 16:22:38 +0700

    angular js not ready

--- application/controllers/belajar.php
+++ application/controllers/belajar.php
@@ -5,9 +5,8 @@ class Belajar extends CI_Controller {
 	
 	function __construct(){
 		parent::__construct();	
-		$this->load->model('m_data','tb_bookmark');
 
-		
+
 		 //----JTable 08/06/2016-----//	
 		// $this->load->model("m_data", 'get_db');
 		// $this->load->model("m_data_user",'get_db_u');
@@ -23,85 +22,8 @@ class Belajar extends CI_Controller {
 		}
 	}
 
-	public function bookmark()
-    {
-        $this->load->helper('url');
-        $this->load->view('v_header');
-        $this->load->view('v_bookmark');
-    }
- 
-    public function ajax_list()
-    {
-        $list = $this->tb_bookmark->get_datatables();
-        $data = array();
-        $no = $_POST['start'];
-        foreach ($list as $u) {
-            $no++;
-            $row = array();
-            $row[] = $u->firstName;
-            $row[] = $u->lastName;
-            $row[] = $u->gender;
-            $row[] = $u->address;
-            $row[] = $u->dob;
- 
-            //add html for action
-            $row[] = '<a class="btn btn-sm btn-primary" href="javascript:void(0)" title="Edit" onclick="edit_person('."'".$u->id."'".')"><i class="glyphicon glyphicon-pencil"></i> Edit</a>
-                  <a class="btn btn-sm btn-danger" href="javascript:void(0)" title="Hapus" onclick="delete_person('."'".$u->id."'".')"><i class="glyphicon glyphicon-trash"></i> Delete</a>';
- 
-            $data[] = $row;
-        }
- 
-        $output = array(
-                        "draw" => $_POST['draw'],
-                        "recordsTotal" => $this->tb_bookmark->count_all(),
-                        "recordsFiltered" => $this->tb_bookmark->count_filtered(),
-                        "data" => $data,
-                );
-        //output to json format
-        echo json_encode($output);
-    }
- 
-    public function ajax_edit($id)
-    {
-        $data = $this->person->get_by_id($id);
-        echo json_encode($data);
-    }
- 
-    public function ajax_add()
-    {
-        $data = array(
-                'firstName' => $this->input->post('firstName'),
-                'lastName' => $this->input->post('lastName'),
-                'gender' => $this->input->post('gender'),
-                'address' => $this->input->post('address'),
-                'dob' => $this->input->post('dob'),
-            );
-        $insert = $this->person->save($data);
-        echo json_encode(array("status" => TRUE));
-    }
- 
-    public function ajax_update()
-    {
-        $data = array(
-                'firstName' => $this->input->post('firstName'),
-                'lastName' => $this->input->post('lastName'),
-                'gender' => $this->input->post('gender'),
-                'address' => $this->input->post('address'),
-                'dob' => $this->input->post('dob'),
-            );
-        $this->person->update(array('id' => $this->input->post('id')), $data);
-        echo json_encode(array("status" => TRUE));
-    }
- 
-    public function ajax_delete($id)
-    {
-        $this->person->delete_by_id($id);
-        echo json_encode(array("status" => TRUE));
-    }
- 
+
 }
-
-
 
 	//------ JTable 08/06/2016--------------// 
 	/*
@@ -438,4 +360,4 @@ class Belajar extends CI_Controller {
 		$this->session->session_destroy();
 		redirect (base_url('/belajar/login/logout'));
 	} */
-//}
+
